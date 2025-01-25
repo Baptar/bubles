22976856e730c55338c549c5d296f5d3cab54d86
SebastienMorris <sebw.morris@gmail.com>
2025-01-25 15:58:19 +0100

    add time delta time

--- Assets/Script/Movable.cs
+++ Assets/Script/Movable.cs
@@ -32,7 +32,7 @@ public class Movable : MonoBehaviour
                 //Vector3 hitPos = Input.mousePosition;
                 //Vector3 dir = transform.position - hitPos;
                 //Vector3 force = Vector3.Project(mouseVelocity, dir);
-                CursorHit(new Vector2(mouseVelocity.x, mouseVelocity.y));
+                CursorHit(new Vector2(mouseVelocity.x, mouseVelocity.y) * Time.fixedDeltaTime * 100f);
                 //CursorHit(new Vector2(force.x, force.y));
                 _mouseOn = true;
             }
