72788940035f84c6352383037418048b1a4c5844
Baptiste Ruberte <ruberte43@gmail.com>
2025-01-25 04:00:49 +0100

    bb pablo

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -2,6 +2,14 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    [SerializeField] private int numberMissionChild = 5;
+    [SerializeField] private int numberMissionTeenager = 3;
+    [SerializeField] private PlayerBubble playerBubble;
+    
+    private int age = 0;
+    private int actualMissionChild = 0;
+    private int actualMissionTeenager = 0;
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -12,5 +20,19 @@ public class GameManager : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public void SetAge(int age)
+    {
+        this.age = age;
+        
+    }
+    
+    public void RestartGame()
+    {
+        SetAge(0);
+        actualMissionChild = 0;
+        actualMissionTeenager = 0;
+        playerBubble.KillPlayer();
     }
 }
