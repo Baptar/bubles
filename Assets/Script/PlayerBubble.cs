ec5d1a0671c67c56bc75c5a1e766620a3697bcab
Baptiste Ruberte <ruberte43@gmail.com>
2025-01-25 03:12:23 +0100

    bb seb

--- Assets/Script/PlayerBubble.cs
+++ Assets/Script/PlayerBubble.cs
@@ -1,16 +1,45 @@
+using Microsoft.Unity.VisualStudio.Editor;
 using UnityEngine;
+using UnityEngine.U2D;
 
-public class PlayerBubble : MonoBehaviour
+public class PlayerBubble : Movable
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private string name;
+    private int accessoryIndex = 0;
+    private int colorIndex = 0;
+    
+    public Sprite[][] sprites; // first tab is for accessorie and under it is color
+    public Transform spawnTransform;
+
     void Start()
     {
-        
+        GetComponent<SpriteRenderer>().enabled = false;
+    }
+    
+    public void SetName(string name) { this.name = name; }
+
+    public void SetColorIndex(int colorIndex)
+    {
+        this.colorIndex = colorIndex;
+        SetPlayerSprite(colorIndex, accessoryIndex);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void SetAccessoryIndex(int accessoryIndex)
     {
-        
+        this.accessoryIndex = accessoryIndex; 
+        SetPlayerSprite(colorIndex, accessoryIndex);
+    }
+    
+    private void SetPlayerSprite(int colorIndex, int accessoryIndex) { GetComponent<SpriteRenderer>().sprite = sprites[accessoryIndex][colorIndex]; }
+
+    public void SpawnPlayer()
+    {
+        GetComponent<SpriteRenderer>().enabled = true;
+    }
+
+    public void RespawnPlayer()
+    {
+        GetComponent<SpriteRenderer>().enabled = false;
+        transform.position = spawnTransform.position;
     }
 }
